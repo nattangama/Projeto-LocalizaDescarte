eb885b226468e264e50cc716f0aa07ec4d8ba1c3
Nattan Gama <91083215+NattanGama@users.noreply.github.com>
2022-02-06 00:39:02 -0300

    Create LojaDAO.java

---
+++ src/main/java/br/start/localiza/dao/LojaDAO.java
@@ -0,0 +1,20 @@
+package br.start.localiza.dao;
+
+import java.util.List;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import br.start.localiza.model.Loja;
+
+public interface LojaDAO extends JpaRepository<Loja, String> {
+	
+	@Query(value =  "select * from loja where cnpj = ?1 AND senha_loja = ?2", nativeQuery = true)
+	Loja loginLoja(String cnpj, String senha);
+	
+	@Query(value = "select * from loja where nome_loja = ?1 ORDER BY RAND(cnpj) ", nativeQuery = true)
+	List<Loja> buscarPorNome(String nome, String cnpj);
+	
+	
+}
